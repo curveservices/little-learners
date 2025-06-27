b9867df9087c1024ce79d70a8a46543d0d598d85
Paul <p.rossiter83@gmail.noreply.github.com>
2025-06-27 12:01:09 +0100

    Initialcommit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React, { Suspense } from 'react';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import Layout from './layout/main/index.jsx'
+import './App.scss';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Layout />}>
+        <Route index
+          element={
+          <Suspense fallback={<LoadingSpinner />}>
+            <Home />
+          </Suspense>
+        }
+        />
+        </Route>
+    </>,
+  ),
+);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+}
+
+export default App
+
