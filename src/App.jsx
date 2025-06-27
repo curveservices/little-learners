76c37ac6a6d244eae0eadb30c53909120d629456
Paul <p.rossiter83@gmail.noreply.github.com>
2025-06-27 15:22:57 +0100

    Refactor App.jsx for improved readability and organization

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,35 @@
-import React, { Suspense } from 'react';
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
-import Layout from './layout/main/index.jsx'
-import './App.scss';
+import React, { Suspense } from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from "./layout/main/index.jsx";
+import LoadingSpinner from "./components/loadingSpinner/index.jsx";
+import Home from "./pages/home/index.jsx";
+
+import "./App.scss";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" element={<Layout />}>
-        <Route index
+        <Route
+          index
           element={
-          <Suspense fallback={<LoadingSpinner />}>
-            <Home />
-          </Suspense>
-        }
+            <Suspense fallback={<LoadingSpinner />}>
+              <Home />
+            </Suspense>
+          }
         />
-        </Route>
+      </Route>
     </>,
   ),
 );
 
 const App = () => {
   return <RouterProvider router={router} />;
-}
+};
 
-export default App
-
+export default App;
