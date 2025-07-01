08aa4eb70bf86c1d254567488422bcc76133cf90
Paul <p.rossiter83@gmail.noreply.github.com>
2025-07-01 16:08:16 +0100

    Refactor Helmet component for improved code formatting and readability

--- src/components/helmet/index.jsx
+++ src/components/helmet/index.jsx
@@ -1,34 +1,35 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 
 const Helmet = ({ title, description, keywords, schemaMarkup }) => {
-    useEffect(() => {
-        if (title) document.title = title;
-        if (description) {
-            let metaDesc = document.querySelector("meta[name='description']");
-            if (!metaDesc) {
-                metaDesc = document.createElement("meta");
-                metaDesc.name = "description";
-                document.head.appendChild(metaDesc);
-            }
-            metaDesc.content = description;
-        }
-        if (keywords) {
-            let metaKeywords = document.querySelector("meta[name='keywords']");
-            if (!metaKeywords) {
-                metaKeywords = document.createElement("meta");
-                metaKeywords.name = "keywords";
-                document.head.appendChild(metaKeywords);
-            }
-            metaKeywords.content = keywords;
-        }
-        if (schemaMarkup) {
-            let scriptTag = document.querySelector("script[type='application/ld+json']");
-            if (scriptTag) scriptTag.remove();
-        };
-    }, [title, description, keywords, schemaMarkup]);
+  useEffect(() => {
+    if (title) document.title = title;
+    if (description) {
+      let metaDesc = document.querySelector("meta[name='description']");
+      if (!metaDesc) {
+        metaDesc = document.createElement("meta");
+        metaDesc.name = "description";
+        document.head.appendChild(metaDesc);
+      }
+      metaDesc.content = description;
+    }
+    if (keywords) {
+      let metaKeywords = document.querySelector("meta[name='keywords']");
+      if (!metaKeywords) {
+        metaKeywords = document.createElement("meta");
+        metaKeywords.name = "keywords";
+        document.head.appendChild(metaKeywords);
+      }
+      metaKeywords.content = keywords;
+    }
+    if (schemaMarkup) {
+      let scriptTag = document.querySelector(
+        "script[type='application/ld+json']",
+      );
+      if (scriptTag) scriptTag.remove();
+    }
+  }, [title, description, keywords, schemaMarkup]);
 
-    return null;
+  return null;
 };
 
-export default Helmet
-
+export default Helmet;
