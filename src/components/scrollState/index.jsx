b1ce8c208951552370257a91e6671499a327b9e7
Paul <p.rossiter83@gmail.noreply.github.com>
2025-07-01 16:08:41 +0100

    Refactor useScrollStates component for improved code consistency and readability

--- src/components/scrollState/index.jsx
+++ src/components/scrollState/index.jsx
@@ -1,27 +1,26 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 const useScrollStates = () => {
   const [second, setSecond] = useState(false);
   const [third, setThird] = useState(false);
   const [forth, setForth] = useState(false);
-  const [fith, setFith] = useState(false)
+  const [fith, setFith] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
       const scrollY = window.scrollY;
-      setSecond(scrollY > 50);
-      setThird(scrollY > 1200);
+      setSecond(scrollY > 150);
+      setThird(scrollY > 1000);
       setForth(scrollY > 1400);
       setFith(scrollY > 2100);
     };
 
-    window.addEventListener('scroll', handleScroll);
-    
-    return () => window.removeEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return { second, third, forth, fith };
 };
 
 export default useScrollStates;
-
