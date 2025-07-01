7b4c82b4a56b5d991a70d8c65a210eef0ab2edb1
Paul <p.rossiter83@gmail.noreply.github.com>
2025-07-01 16:06:58 +0100

    Add routes for About, Learning & Playing, Gallery, and Contact pages

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,14 @@ import {
 } from "react-router-dom";
 import Layout from "./layout/main/index.jsx";
 import LoadingSpinner from "./components/loadingSpinner/index.jsx";
-import NotFound from "./pages/404"
+import NotFound from "./pages/404";
 import Home from "./pages/home/index.jsx";
 
 import "./App.scss";
+import About from "./pages/about/index.jsx";
+import LearninPlaying from "./pages/learning/index.jsx";
+import Gallery from "./pages/gallery/index.jsx";
+import Contact from "./pages/contact/index.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -23,6 +27,30 @@ const router = createBrowserRouter(
               <Home />
             </Suspense>
           }
+        />
+        <Route path="/about-us" element={
+          <Suspense fallback={<LoadingSpinner />}>
+            <About />
+          </Suspense>
+        }
+        />
+        <Route path="/learning-and-playing" element={
+            <Suspense fallback={<LoadingSpinner />}>
+              <LearninPlaying />
+            </Suspense>
+        }
+        />
+        <Route path="/gallery" element={
+          <Suspense fallback={<LoadingSpinner />}>
+            <Gallery />
+          </Suspense>
+        }
+        />
+        <Route path="/contact-us" element={
+          <Suspense fallback={<LoadingSpinner />}>
+            <Contact />
+          </Suspense>
+        }
         />
       </Route>
       <Route
