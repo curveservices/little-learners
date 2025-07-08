fe3d78d962417f2d2ffdc6f8db80ad00042bcd7a
Paul <p.rossiter83@gmail.noreply.github.com>
2025-07-08 15:08:15 +0100

    Enhance TextAndImage component with additional paragraph support and improved flip effect on hover

--- src/components/blurImage/index.jsx
+++ src/components/blurImage/index.jsx
@@ -7,13 +7,18 @@ const TextAndImage = ({
   p1,
   p2,
   p3,
+  p4,
   text,
   link,
   img,
+  display = true,
   reverse = false,
+  flip = false,
 }) => {
   return (
-    <div className={`text-image-wrapper ${reverse ? "reverse" : ""}`}>
+    <div
+      className={`text-image-wrapper ${flip ? "flip" : ""} ${reverse ? "reverse" : ""}`}
+    >
       <div className="text-box">
         <div className="subtitle">
           <div className="indent"></div>
@@ -23,14 +28,18 @@ const TextAndImage = ({
         <p>{p1}</p>
         <p>{p2}</p>
         <p>{p3}</p>
-        <div className="btn-container">
-          <Button text={text} link={link} />
-        </div>
+        <p>{p4}</p>
+        {display && (
+          <div className="btn-container">
+            <Button text={text} link={link} />
+          </div>
+        )}
       </div>
       <img
         src={img}
         alt="Little Learners Pre-School in Blackheath, London"
         className="blur-img"
+        loading="lazy"
       />
     </div>
   );
