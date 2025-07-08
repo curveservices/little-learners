417629a9ae58a3a6d1ba0c44b113b23989acd624
Paul <p.rossiter83@gmail.noreply.github.com>
2025-07-08 15:12:02 +0100

    Add Policies component with hero section and main title for policies and procedures

---
+++ src/pages/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Policies = () => {
+  return (
+    <div className='policy-page'>
+      <section className="hero">
+        <div className="text-box">
+            <div className="subtitle">
+                <div className="indent"></div>
+                documents
+            </div>
+            <h1 className="main-title">Policies and Procedures</h1>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Policies
