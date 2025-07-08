db4cc4d6e8b65b9511f01363b181ac1c9d238050
Paul <p.rossiter83@gmail.noreply.github.com>
2025-07-08 15:09:46 +0100

    Update social media links and refine styles for consistency

--- src/components/socials/index.jsx
+++ src/components/socials/index.jsx
@@ -1,25 +1,18 @@
 import {
   faFacebook,
-  faFacebookF,
-  faGithub,
-  faInstagram,
-  faInstagramSquare,
-  faLinkedin,
-  faLinkedinIn,
   faSquareInstagram,
 } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
-
 import "./index.scss";
 
-const Socials = (props) => {
+const Socials = () => {
   return (
     <>
       <ul className="iconList">
         <li className="icons">
           <Link
-            to="https://www.facebook.com/emma.hollows.design"
+            to="https://www.facebook.com/LittleLearnersPlaygroup"
             target="_blank"
             alt="facebook"
             aria-label="follow me on facebook"
@@ -27,13 +20,12 @@ const Socials = (props) => {
             <FontAwesomeIcon
               icon={faFacebook}
               alt="facebook"
-              color="var(--primary)"
             />
           </Link>
         </li>
         <li className="icons">
           <Link
-            to="https://www.instagram.com/emma.hollows.design/"
+            to="https://www.instagram.com/littlelearnerspreschool2022/"
             target="_blank"
             alt="Instagram"
             aria-label="follow me on instagram"
@@ -41,21 +33,6 @@ const Socials = (props) => {
             <FontAwesomeIcon
               icon={faSquareInstagram}
               alt="Instagram Link"
-              color="var(--primary)"
-            />
-          </Link>
-        </li>
-        <li className="icons">
-          <Link
-            to="https://www.linkedin.com/in/emmalouisehollows/"
-            target="_blank"
-            alt="LinkedIn"
-            aria-label="link with me on LinkedIn"
-          >
-            <FontAwesomeIcon
-              icon={faLinkedin}
-              alt="LinkedIn"
-              color="var(--primary)"
             />
           </Link>
         </li>
