2f7c3da0ceb89eb66f43f861b78955e83dc4403b
Paul <p.rossiter83@gmail.noreply.github.com>
2025-07-11 11:57:59 +0100

    chunksize increased and rollupoptions implemented

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,19 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("react")) return "react";
+            if (id.includes("firebase")) return "firebase";
+            if (id.includes("react-slick")) return "react-slick";
+            return "vendor";
+          }
+        }
+      }
+    }
+  }
 });
