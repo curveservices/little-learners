f1f992170dc9789dde8efd9c21045c02a5d374b7
Paul <p.rossiter83@gmail.noreply.github.com>
2025-07-11 12:40:11 +0100

    map leaflet updated

--- src/components/leafletMap/index.jsx
+++ src/components/leafletMap/index.jsx
@@ -1,28 +1,42 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
+// Fix default icon issue in React-Leaflet
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
+  iconUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+});
+
 const LeafletMap = () => {
-  const mapRef = useRef(null);
-
-  useEffect(() => {
-    const map = L.map(mapRef.current).setView([51.47005, 0.001359], 13);
-
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution: "&copy; OpenStreetMap contributors",
-    }).addTo(map);
-
-    const marker = L.marker([51.459855742997284, 0.0015531659665287039]).addTo(
-      map,
-    );
-    marker.bindPopup("<b>Hello!</b><br>We are here.").openPopup();
-
-    return () => {
-      map.remove();
-    };
-  }, []);
-
-  return <div ref={mapRef} style={{ height: "100vh", width: "100%" }} />;
+  return (
+    <div style={{ height: "100vh", width: "100%" }}>
+      <MapContainer
+        center={[51.47005, 0.001359]}
+        zoom={13}
+        style={{ height: "100%", width: "100%" }}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; OpenStreetMap contributors"
+        />
+        <Marker position={[51.459855742997284, 0.0015531659665287039]}>
+          <Popup>
+            <b>Hello!</b>
+            <br />
+            We are here.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
 };
 
 export default LeafletMap;
