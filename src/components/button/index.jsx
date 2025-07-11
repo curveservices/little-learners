7a852e030d4141a11b0bfb080c24caefbeb4d67e
Paul <p.rossiter83@gmail.noreply.github.com>
2025-07-11 14:12:43 +0100

    props added for extrnal and downloadable links

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -2,19 +2,45 @@ import { Link } from "react-router-dom";
 import "./index.scss";
 
 const Button = (props) => {
+  const {
+    link,
+    text,
+    type,
+    target,
+    background,
+    color,
+    fontSize,
+    display,
+    isInternal = true, 
+    download = false,
+  } = props;
+
   const buttonStyle = {
-    background: props.background,
-    color: props.color,
-    fontSize: props.fontSize + "px",
-    display: props.display,
+    background,
+    color,
+    fontSize: fontSize + "px",
+    display,
   };
 
-  return (
-    <Link to={props.link} type={props.type} target={props.target}>
-      <button style={buttonStyle} className="flat-btn">
-        {props.text}
-      </button>
+  const buttonElement = (
+    <button style={buttonStyle} className="flat-btn" type={type}>
+      {text}
+    </button>
+  );
+
+  return isInternal ? (
+    <Link to={link} target={target}>
+      {buttonElement}
     </Link>
+  ) : (
+    <a
+      href={link}
+      target={target || "_blank"}
+      rel="noopener noreferrer"
+      download={download}
+    >
+      {buttonElement}
+    </a>
   );
 };
 
