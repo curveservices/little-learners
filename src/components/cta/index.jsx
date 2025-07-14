978448c7a007283ec409122b7cb4595d76e94cc6
Paul <p.rossiter83@gmail.noreply.github.com>
2025-07-14 19:14:08 +0100

    image added for consistancy

--- src/components/cta/index.jsx
+++ src/components/cta/index.jsx
@@ -1,3 +1,4 @@
+import aboutBody from "../../assets/images/about-body.webp";
 import Button from "../button";
 import "./index.scss";
 
@@ -12,21 +13,28 @@ const CallToAction = ({
   display,
 }) => {
   return (
-    <div className="cta block">
-      <div className="cta-container">
-        <h3 className="cta-title">{title}</h3>
-        <p className="cta-p">{p1}</p>
-        <p className="cta-p">{p2}</p>
+    <>
+      <img
+        src={aboutBody}
+        alt="Little Learners preschool"
+        className="cta-body"
+      />
+      <div className="cta">
+        <div className="cta-container">
+          <h3 className="cta-title">{title}</h3>
+          <p className="cta-p">{p1}</p>
+          <p className="cta-p">{p2}</p>
+        </div>
+        <div className="btn-container">
+          <Button
+            text={text1}
+            link={link1}
+            background="var(--secondary)"
+            color="#fff"
+          />
+        </div>
       </div>
-      <div className="btn-container">
-        <Button
-          text={text1}
-          link={link1}
-          background="var(--secondary)"
-          color="#fff"
-        />
-      </div>
-    </div>
+    </>
   );
 };
 
